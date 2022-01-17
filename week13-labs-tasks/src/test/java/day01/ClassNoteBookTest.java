6536219eea54c4eb32689ca20d98ac54997dc7a3
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2022-01-17 16:24:06 +0100

    Updated day01.ClassNoteBookTest

--- week13-labs-tasks/src/test/java/day01/ClassNoteBookTest.java
+++ week13-labs-tasks/src/test/java/day01/ClassNoteBookTest.java
@@ -2,12 +2,16 @@ package day01;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClassNoteBookTest {
 
     ClassNoteBook noteBook = new ClassNoteBook();
-    Student student = new Student(10, "Jill Doe");
+    Student student1 = new Student(10, "Jill Doe");
+    Student student2 = new Student(9, "Jack Doe");
 
     @Test
     void testCreate() {
@@ -16,17 +20,25 @@ class ClassNoteBookTest {
 
     @Test
     void testAddStudent() {
-        noteBook.addStudent(student);
+        noteBook.addStudent(student1);
         assertEquals(1, noteBook.getStudentGrades().size());
-        assertEquals(0, noteBook.getStudentGrades().get(student).size());
+        assertEquals(0, noteBook.getStudentGrades().get(student1).size());
+    }
+
+    @Test
+    void testAddStudentInOrder() {
+        noteBook.addStudent(student1);
+        noteBook.addStudent(student2);
+        assertEquals(List.of(student2, student1), new ArrayList<>(noteBook.getStudentGrades().keySet()));
+        assertNotEquals(List.of(student1, student2), new ArrayList<>(noteBook.getStudentGrades().keySet()));
     }
 
     @Test
     void testGradeStudent() {
-        noteBook.addStudent(student);
+        noteBook.addStudent(student1);
         noteBook.gradeStudent(10, 5);
-        assertEquals(1, noteBook.getStudentGrades().get(student).size());
-        assertEquals(5, noteBook.getStudentGrades().get(student).get(0));
+        assertEquals(1, noteBook.getStudentGrades().get(student1).size());
+        assertEquals(5, noteBook.getStudentGrades().get(student1).get(0));
     }
 
     @Test
